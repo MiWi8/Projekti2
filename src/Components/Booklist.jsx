87000bbed688df6a5b8abd1292cec9f58ed74a9e
MiWi8 <144667390+MiWi8@users.noreply.github.com>
2026-01-20 11:39:35 +0200

    fix2

--- src/Components/Booklist.jsx
+++ src/Components/Booklist.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import BookCard from "./Bookcard";
-import booksData from "../Data/Books.json";
+
 
 function BookList() {
   const [books, setBooks] = useState([]);
